a0749767b9de3f40260ca793eb696432b594b1ce
vincentz321 <56129317+vincentz321@users.noreply.github.com>
2019-12-06 10:13:33 -0800

    12/6/19

--- Assets/Scenes/PlayerController.cs
+++ Assets/Scenes/PlayerController.cs
@@ -3,31 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
-{ public float horizontaIInput;
-  public float speed = 10.0f;
-  public float xRange = 10.0f;
-  public GameObject projectilePrefab;
+{
+    public float horizontaIInput;
+    public float speed = 10.0f;
+    public float xRange = 17.0f;
+    public GameObject projectilePrefab;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-      horizontaIInput  = Input.GetAxis("Horizontal");
-      transform.Translate(Vector3.right * horizontaIInput * Time.deltaTime * speed);
-      
-      if (transform.position.x < xRange)
-          transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
-          
+        horizontaIInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontaIInput * Time.deltaTime * speed);
 
-          if (transform.position.x > xRange)
-          transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
-          
-          if (Input.GetKeyDown(KeyCode.Space))
-          
-          Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        if (transform.position.x < -xRange)
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+
+        if (transform.position.x > xRange)
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
     }
 }
